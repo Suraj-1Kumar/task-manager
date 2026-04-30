76760ed47bd870b88c3286909f1509ab259540f4
Suraj-1Kumar <surajkumarsharma516@gmail.com>
2026-05-01 00:58:13 +0530

    chore: setup environment variables for cloud deployment

--- backend/src/main/java/com/taskmanager/security/WebSecurityConfig.java
+++ backend/src/main/java/com/taskmanager/security/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.taskmanager.security;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
